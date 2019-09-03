77da5786ddffc803c8988f626335fe442b756689
Paschalis Frangidis <pascal.frag@gmail.com>
2019-09-03 14:28:46 +0300

    Initial commit

---
+++ program.py
@@ -0,0 +1,22 @@
+import PySimpleGUI as sg
+
+
+def main():
+    # All the stuff inside your window. This is the PSG magic code compactor...
+    layout = [[sg.Text('Some text on Row 1')],
+              [sg.Text('Enter something on Row 2'), sg.InputText()],
+              [sg.OK(), sg.Cancel()]]
+
+    # Create the Window
+    window = sg.Window('Window Title', layout)
+    # Event Loop to process "events"
+    while True:
+        event, values = window.Read()
+        if event in (None, 'Cancel'):
+            break
+
+    window.Close()
+
+
+if __name__ == '__main__':
+    main()
